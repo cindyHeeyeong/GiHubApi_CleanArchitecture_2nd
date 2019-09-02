0cb176800984631d944b6a179a035c2cf328f391
cindyheeyeong <gohy12@naver.com>
2019-09-02 19:38:53 +0900

    ddd

---
+++ app/src/main/java/com/example/cleanarchitecture_2nd/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.cleanarchitecture_2nd.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
